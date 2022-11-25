69b10024d7a873139ad6e303cad1dc844e963052
Alex Platt <alja.platt@gmail.com>
2022-11-25 13:45:15 +0000

    added second route with url params

--- loeber/main.py
+++ loeber/main.py
@@ -4,6 +4,8 @@ from typing import Union
 app = FastAPI()
 
 @app.get("/")
+# can be async 
+# async def read_root():
 def read_root():
     # dictionary auto converted to json 
     return {"Hello": "World"}
@@ -15,3 +17,5 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# http://localhost:8000/items/5?q=hello-world
