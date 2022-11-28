b9733a55177ea581c46bf677a1fa63fa3f2ab323
Alex Platt <alja.platt@gmail.com>
2022-11-28 11:08:46 +0000

    added fastapi tests

--- loeber/main.py
+++ loeber/main.py
@@ -1,21 +1,60 @@
-from fastapi import FastAPI
-from typing import Union
+# from fastapi import FastAPI
+# from typing import Union
+
+# app = FastAPI()
+
+# @app.get("/")
+# # can be async 
+# # def read_root():
+# async def read_root():
+#     # dictionary auto converted to json 
+#     return {"Hello": "World"}
+
+
+# #? run with:
+# #* uvicorn main:app --reload
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+# # http://localhost:8000/items/5?q=hello-world
+
+#* extended 
+
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
+
+fake_secret_token = "coneofsilence"
+
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+}
 
 app = FastAPI()
 
-@app.get("/")
-# can be async 
-# async def read_root():
-def read_root():
-    # dictionary auto converted to json 
-    return {"Hello": "World"}
+
+class Item(BaseModel):
+    id: str
+    title: str
+    description: str | None = None
 
 
-#? run with:
-#* uvicorn main:app --reload
+@app.get("/items/{item_id}", response_model=Item)
+async def read_main(item_id: str, x_token: str = Header()):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return fake_db[item_id]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
-# http://localhost:8000/items/5?q=hello-world
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item, x_token: str = Header()):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item.id in fake_db:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    fake_db[item.id] = item
+    return item
